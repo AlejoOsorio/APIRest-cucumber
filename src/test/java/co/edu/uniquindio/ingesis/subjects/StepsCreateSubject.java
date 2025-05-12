238b0cd01ba8a133ecd328ffb5cc6b4d1e96d2eb
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2025-05-11 23:16:20 -0500

    Se organizaron unos detalles

--- src/test/java/co/edu/uniquindio/ingesis/subjects/StepsCreateSubject.java
+++ src/test/java/co/edu/uniquindio/ingesis/subjects/StepsCreateSubject.java
@@ -33,27 +33,6 @@ public class StepsCreateSubject {
         context.setResponse(response);
     }
 
-    @When("el cliente envía una solicitud POST con una materia válida")
-    public void elClienteEnvíaUnaSolicitudPOSTConUnaMateriaVálida() {
-        Map<String, Object> subjectMap = new HashMap<>();
-        subjectMap.put("name", "Programación I");
-        subjectMap.put("description", "Introducción a la programación en Java");
-        subjectMap.put("credits", 3);
-
-        response = given()
-                .spec(ApiConfig.BASE_REQUEST)
-                .body(subjectMap)
-                .when()
-                .post("/subjects");
-
-        response.body().print();
-        context.setResponse(response);
-    }
-
-    @And("el cuerpo de la respuesta debe contener los detalles de la materia creada")
-    public void elCuerpoDeLaRespuestaDebeContenerLosDetallesDeLaMateriaCreada() {
-        response.then().body(matchesJsonSchemaInClasspath("schemes/subject-scheme.json"));
-    }
 
     @When("el cliente envía una solicitud POST con el nombre vacío")
     public void elClienteEnvíaUnaSolicitudPOSTConElNombreVacío() {
--- src/test/resources/co/edu/uniquindio/ingesis/subject/create-subject.feature
+++ src/test/resources/co/edu/uniquindio/ingesis/subject/create-subject.feature
@@ -3,12 +3,6 @@ Feature: Crear una materia
   Quiero poder crear nuevas materias (subjects)
   Para agregarlas al sistema académico
 
-  Scenario: Crear una materia con valores válidos exitosamente
-    Given El sistema está listo para aceptar nuevas materias
-    When el cliente envía una solicitud POST con una materia válida
-    Then la respuesta debe tener el código de estado 201
-    And el cuerpo de la respuesta debe contener los detalles de la materia creada
-
   Scenario: Intentar crear una materia con nombre vacío
     Given El sistema está listo para aceptar nuevas materias
     When el cliente envía una solicitud POST con el nombre vacío
